54296f6bebd3bf90271b9793254b429967f92506
BrianSanti <briansanti80@gmail.com>
2021-02-22 17:48:16 -0600

    Verificar

--- SGP/CapaVista_SGP/frmVerificar.Designer.cs
+++ SGP/CapaVista_SGP/frmVerificar.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.pnl_cita = new System.Windows.Forms.Panel();
             this.btnSalir = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_cita = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtEstadoBoleto = new System.Windows.Forms.TextBox();
             this.panel3.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -47,66 +48,67 @@
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(36)))), ((int)(((byte)(48)))));
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel3.Controls.Add(this.panel1);
-            this.panel3.Location = new System.Drawing.Point(34, 63);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Location = new System.Drawing.Point(45, 78);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(534, 302);
+            this.panel3.Size = new System.Drawing.Size(711, 371);
             this.panel3.TabIndex = 13;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(169)))));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.txtEstadoBoleto);
+            this.panel1.Controls.Add(this.pnl_cita);
             this.panel1.Controls.Add(this.btnSalir);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.txt_cita);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(16, 17);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(21, 21);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(500, 265);
+            this.panel1.Size = new System.Drawing.Size(665, 325);
             this.panel1.TabIndex = 7;
             // 
-            // panel4
+            // pnl_cita
             // 
-            this.panel4.BackColor = System.Drawing.Color.Red;
-            this.panel4.Location = new System.Drawing.Point(311, 166);
-            this.panel4.Margin = new System.Windows.Forms.Padding(2);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(68, 42);
-            this.panel4.TabIndex = 8;
+            this.pnl_cita.BackColor = System.Drawing.Color.Red;
+            this.pnl_cita.Location = new System.Drawing.Point(415, 204);
+            this.pnl_cita.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnl_cita.Name = "pnl_cita";
+            this.pnl_cita.Size = new System.Drawing.Size(91, 52);
+            this.pnl_cita.TabIndex = 8;
             // 
             // btnSalir
             // 
             this.btnSalir.BackColor = System.Drawing.Color.White;
             this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(282, 91);
-            this.btnSalir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSalir.Location = new System.Drawing.Point(376, 112);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(128, 32);
+            this.btnSalir.Size = new System.Drawing.Size(171, 39);
             this.btnSalir.TabIndex = 7;
             this.btnSalir.Text = "Verificar";
             this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
-            // textBox1
+            // txt_cita
             // 
-            this.textBox1.Location = new System.Drawing.Point(238, 42);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(222, 20);
-            this.textBox1.TabIndex = 6;
-            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.txt_cita.Location = new System.Drawing.Point(317, 52);
+            this.txt_cita.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_cita.Name = "txt_cita";
+            this.txt_cita.Size = new System.Drawing.Size(295, 22);
+            this.txt_cita.TabIndex = 6;
+            this.txt_cita.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(36)))), ((int)(((byte)(48)))));
-            this.label3.Location = new System.Drawing.Point(27, 166);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(36, 204);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(162, 31);
+            this.label3.Size = new System.Drawing.Size(195, 38);
             this.label3.TabIndex = 5;
             this.label3.Text = "Estado Cita ";
             // 
@@ -115,10 +117,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(36)))), ((int)(((byte)(48)))));
-            this.label2.Location = new System.Drawing.Point(27, 30);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(36, 37);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(166, 31);
+            this.label2.Size = new System.Drawing.Size(200, 38);
             this.label2.TabIndex = 4;
             this.label2.Text = "Numero Cita";
             // 
@@ -129,9 +130,9 @@
             this.panel2.Controls.Add(this.label1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(600, 45);
+            this.panel2.Size = new System.Drawing.Size(800, 54);
             this.panel2.TabIndex = 12;
             // 
             // label1
@@ -139,23 +140,32 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(36)))), ((int)(((byte)(48)))));
-            this.label1.Location = new System.Drawing.Point(2, 5);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(3, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 31);
+            this.label1.Size = new System.Drawing.Size(137, 38);
             this.label1.TabIndex = 2;
             this.label1.Text = "Verificar";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // txtEstadoBoleto
+            // 
+            this.txtEstadoBoleto.Enabled = false;
+            this.txtEstadoBoleto.Location = new System.Drawing.Point(317, 123);
+            this.txtEstadoBoleto.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEstadoBoleto.Name = "txtEstadoBoleto";
+            this.txtEstadoBoleto.Size = new System.Drawing.Size(20, 22);
+            this.txtEstadoBoleto.TabIndex = 24;
+            this.txtEstadoBoleto.Visible = false;
+            // 
             // frmVerificar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(175)))), ((int)(((byte)(135)))));
-            this.ClientSize = new System.Drawing.Size(600, 380);
+            this.ClientSize = new System.Drawing.Size(800, 468);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmVerificar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmVerificar";
@@ -176,8 +186,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txt_cita;
+        private System.Windows.Forms.Panel pnl_cita;
         private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtEstadoBoleto;
     }
 }
